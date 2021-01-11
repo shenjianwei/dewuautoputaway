2d9ba0403fa81c5723a11f1531172ba8a05b82da
doooyf <454048833@qq.com>
2021-01-11 22:48:55 +0800

    添加请求线程，整理请求token获取逻辑

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import math
+import os
 import sys
 import threading
 import requests
@@ -8,6 +9,13 @@ import time
 import hashlib
 import tkinter as tk
 from tkinter import Menu, Toplevel, messagebox
+from selenium import webdriver
+from selenium.webdriver import common
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pynput.mouse import Button, Controller as c1
+# from pynput.keyboard import Key, Controller as c2
 
 
 class App:
@@ -23,12 +31,15 @@ class App:
     token = ""  # Token值
     url = 'https://stark.dewu.com'  # 请求域名
     api = url + '/api/v1/h5/biz'  # 请求地址
+    dewuTokenFilePath = "./dewuToken.txt"
     # 请求相关工具参数
     dewuRequestMax = 3  # 请求最大次数
-    dewuRequestWaitTime = 5
+    dewuRequestWaitTime = 5  # 请求等待间隔时间
+    dewuRequestAgainToken = False  # 重新获取token机会
 
     # 元素
     tokenEntry = ""  # 文本输入框 Token 值
+    logTextDom = ""  # 日志 text 框
 
     def __init__(self):
         self.initLogging()  # 初始化日志
@@ -62,14 +73,13 @@ class App:
         self.saleLowGoodsListText.pack(padx=3, pady=0, side="right", fill="both", expand="yes")
 
         tk.Label(self.root, text="执行日志：", anchor="w").pack(side="top", fill="x")
-        self.text = text = tk.Text(self.root, width=100)  # 执行日志文本
-
-        text.pack(side="top", expand="yes", fill="both")
+        self.logTextDom = tk.Text(self.root, width=100)  # 执行日志文本
+        self.logTextDom.pack(side="top", expand="yes", fill="both")
 
         # 菜单
         menu = Menu(self.root)
         menus = Menu(menu, tearoff=0)
-        menus.add_command(label="设置自动监测间隔时间(秒)", command=lambda: thread_it(topSetInterval))
+        menus.add_command(label="设置自动监测间隔时间(秒)")
         menu.add_cascade(label="设置", menu=menus)
         menu.add_command(label='关于', command=self.about)
         self.root.config(menu=menu)
@@ -82,79 +92,215 @@ class App:
         self.startBtn.pack(padx=15, pady=10, side="left", fill="both", expand="yes")
         # button1.grid(row=0, column=1)
         tk.Button(frameBtn, text="结束执行", ).pack(padx=15, pady=10, side="right", fill="both", expand="yes")
-        # self.getToken()
+
+        # 初始化获得Token
+        self.thread_it(App.getToken, self)
         self.root.mainloop()
 
-    """
-    关于产品
-    """
+    # self.getToken()
+    def thread_it(self, func, *args):
+        """
+        将函数打包进线程
+        :param func: 方法（Class.func）
+        :param args: 参数 (self|*args)
+        :return:
+        """
+        # 创建
+        t = threading.Thread(target=func, args=args)
+        # 守护 !!!
+        t.setDaemon(True)
+        # 启动
+        t.start()
+        # 阻塞--卡死界面！
+        # t.join()
 
     def about(self):
+        """
+        关于产品 - 弹窗提示
+        """
         messagebox.showinfo("关于", self.appVersions)
 
-    """
-    设置顶部信息
-    """
-
     def setInfo(self):
+        """
+        设置顶部信息
+        :return: String
+        """
         #                           enterDeposit                            interval
         return "保证金：" + str(self.enterDeposit) + " ；执行间隔 " + str(self.intervalTime) + "秒/次；"
 
-    """
-    获取Token
-    
-    文本无值从当前目录的dewuToken.txt文件获取token
-    token 失效则自动打开浏览器获取
-    """
-
     def getToken(self):
+        """
+        获取Token
+
+        文本无值从当前目录的dewuToken.txt文件获取token
+        token 失效则自动打开浏览器获取
+        :return: token|False
+        """
         token = self.tokenEntry.get()
         if token == "":  # 文本中token为空值
             try:
-                with open('./dewuToken.txt', 'r', encoding='UTF-8') as f:
+                with open(self.dewuTokenFilePath, 'r', encoding='UTF-8') as f:
                     token = f.read()
                     f.close()
                     self.tokenEntry.insert("0", token)  # 回显token到文本
             except:
-                messagebox.showerror("消息", "读取本地文件dewuToken.txt失败：" + str(sys.exc_info()[0]))
+                messagebox.showerror("消息", "读取本地文件" + self.dewuTokenFilePath + "失败：" + str(sys.exc_info()[0]))
                 return False
         else:
-            with open('./dewuToken.txt', 'w', encoding='UTF-8') as f:
+            with open(self.dewuTokenFilePath, 'w', encoding='UTF-8') as f:
                 f.write(token)
                 f.close()
 
         self.token = token
         # 请求判断是否有效token
-        res = self.get('/home/merchantInfo', '')
-        if res['code'] == 200:
-            return token
-        else:  # 重新自动登录
-            # self.textLog("获取token失败,尝试重新获取，3秒后执行","error")
-            # time.sleep(3)
-            yesNo = messagebox.askyesno("消息", "获取token失败,是否执行重新登录")
-            if yesNo:
-                return False
+        res = self.dewuRequest('get', '/home/merchantInfo', '')
+        if res:
+            if res['code'] == 200:
+                return token
+
+        return False
+
+    def authLogin(self):
+        """
+        打开浏览器登录获取token
+        :return: token|""
+        """
+        mouse = c1()
+        url = self.url + '/business/login.html'
+        options = webdriver.ChromeOptions()
+        # 设置浏览器初始 位置x,y & 宽高x,y
+        # 不加载图片,加快访问速度
+        options.add_experimental_option(
+            "prefs", {"profile.managed_default_content_settings.images": 2})
+        # 设置中文
+        options.add_argument('lang=zh_CN.UTF-8')
+        # 关闭自动测试状态显示
+        # 此步骤很重要，设置为开发者模式，防止被各大网站识别出来使用了Selenium
+        options.add_experimental_option(
+            'excludeSwitches', ['enable-automation'])
+        # 关闭开发者模式
+        options.add_experimental_option("useAutomationExtension", False)
+        # 添加本地代理
+        # options.add_argument("--proxy--server=127.0.0.1:8080")
+        # 添加UA
+        ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+        options.add_argument('user-agent=' + ua)
+
+        driver = webdriver.Chrome(
+            executable_path="./lib/chromedriver.exe", options=options)
+        # 通过浏览器的dev_tool在get页面钱将.webdriver属性改为"undefined"
+        driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            "source": """Object.defineProperty(navigator, 'webdriver', {get: () => undefined})""",
+        })
+        driver.maximize_window()
+        WebDriverWait(driver, 10)
+        driver.get(url)
+        while True:
+            time.sleep(2)
+            mouse.position = (1450, 370)
+            mouse.press(Button.left)
+            mouse.move(1450, 373)
+            time.sleep(2)
+            mouse.release(Button.left)
+            WebDriverWait(driver, 5, 0.5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'nc-lang-cnt')))
+            if driver.find_element_by_class_name('nc-lang-cnt').text == '验证通过':
+                break
             else:
-                return '0'
+                driver.refresh()
+
+        time.sleep(1)
+        driver.find_element_by_xpath(
+            '//*[@placeholder="请输入手机号码"]').send_keys(15757395822)  # 15355979998
+        driver.find_element_by_xpath(
+            '//*[@placeholder="请输入密码"]').send_keys('Dyf131452')  # jeefNO1
+        driver.find_element_by_class_name('el-button').click()
+        time.sleep(3)
+        cookies = driver.get_cookies()  # Selenium为我们提供了get_cookies来获取登录cookies
+        driver.close()  # 获取cookies便可以关闭浏览器
+        # 获取cookies中的token
+        token = self.getJsonToken(cookies, "mchToken")
+        self.openFile(self.dewuTokenFilePath, 'w', token)  # 写入token文件中
+        return token
+
+    def getJsonToken(self, tokenJson, tokenKey):
+        """
+        获取json中的token值
+        :param tokenJson: Json参数
+        :param tokenKey: 键
+        :return: token|""
+        """
+        # tokenJson = json.loads(tokenJson)
+        tokenValue = ""
+        for jsonItem in tokenJson:
+            if jsonItem['name'] == tokenKey:
+                tokenValue = jsonItem['value']
+        return tokenValue
 
     """
-    # ============================ Logging 文本日志 ============================
+    # ============================ Helprs 帮助函数 ============================
     """
 
-    # 初始化日志
     def initLogging(self):
+        """
+        初始化日志
+        :return: 无
+        """
         LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"  # 日志格式化输出
         DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"  # 日期格式
+        self.openPath("./log")
         fp = logging.FileHandler(
-            'log/log_' + time.strftime("%Y_%m_%d", time.localtime()) + '.txt', encoding='utf-8')
+            './log/log_' + time.strftime("%Y_%m_%d", time.localtime()) + '.txt', encoding='utf-8')
         fs = logging.StreamHandler()
         logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT, datefmt=DATE_FORMAT, handlers=[fp, fs])  # 调用
+
+    def openFile(self, path, action, content=""):
+        """
+        打开文件操作，包括写入读取
+        :param path: 请求路径
+        :param action: 文件操作动作
+        :param content: 写入内容内容
+        :return: String - 返回文件读取内容或写入内容
+        """
+        text = str(content)
+        global f
+        try:
+            def wFile():
+                f = open(path, "w", encoding='UTF-8')
+                f.write(text)
+
+            if action == "w":
+                wFile()
+            # if action == "r":
+            else:
+                # 没有文件，则新建空白文件
+                if not os.path.isfile(path):
+                    wFile()
+
+                f = open(path, "r", encoding='UTF-8')
+                text = f.read()
+
+        finally:
+            if f:
+                f.close()
+        return text
+
+    def openPath(self, path):
+        if not os.path.exists(path):
+            os.makedirs(path)
 
     """
     # ============================ text log 文本日志 ============================
     """
+
     def textLog(self, log, colorType="def"):
-        self.text.tag_add('tag', "end")
+        """
+        文本日志输出显示
+        :param log: 日志内容
+        :param colorType: 日志类型：error、success、info、sub、warning
+        :return:
+        """
+        self.logTextDom.tag_add('tag', "end")
         if colorType == "error":
             tag = 'tag_error'
             color = '#ed4014'
@@ -174,20 +320,24 @@ class App:
             tag = 'tag_def'
             color = '#000000'
 
-        self.text.tag_add(tag, "end")
-        self.text.tag_config(tag, foreground=color)
-        self.text.insert("end", "[" + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "]=====>" + str(
+        self.logTextDom.tag_add(tag, "end")
+        self.logTextDom.tag_config(tag, foreground=color)
+        self.logTextDom.insert("end", "[" + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "]=====>" + str(
             log) + '\n',
-                         tag)
-        self.text.see('end')
+                               tag)
+        self.logTextDom.see('end')
         self.root.update()
 
     """
     # ============================ sign 签名 ============================
     """
 
-    # 生成字符串
     def __arrayToString(self, array):
+        """
+        生成字符串
+        :param array: 数组
+        :return: String 返回数组的键值拼接
+        """
         strs = ''
         i = 0
         while i < len(array):
@@ -195,14 +345,22 @@ class App:
             i += 1
         return strs
 
-    # 数组排序
     def __arraySort(self, array):
+        """
+        数组排序
+        :param array:
+        :return:
+        """
         array = sorted(array.items())
         return array
 
-    # sign 签名
-    # 数据按照ASCII码排列
     def __returnSign(self, raw_sign_code_str):
+        """
+        加密sign签名
+        数据按照ASCII码排列
+        :param raw_sign_code_str:
+        :return:
+        """
         raw_sign_code_str += '048a9c4943398714b356a696503d2d36'
         # md5原始sign的字符串
         m = hashlib.md5()
@@ -211,6 +369,11 @@ class App:
         return sign
 
     def getSign(self, data=None):
+        """
+        获取签名
+        :param data:
+        :return:
+        """
         if data is None:
             data = dict()
         if (len(data) > 0):
@@ -225,46 +388,63 @@ class App:
     # ============================ request 请求 ============================
     """
 
-    # 请求
-    #                     方式     地址  参数
     def dewuRequest(self, method, url, data, requestCount=0):
+        """
+        请求方法
+        :param method: 请求方式
+        :param url: 请求地址
+        :param data: 请求参数
+        :param requestCount: 重新尝试请求次数
+        :return:
+        """
         if method == "POST" or method == "post":
             response = self.__post(url, data)
         else:
             response = self.__get(url, data)
 
+        logging.info("[接口][返回]" + response.text)
+
         resData = json.loads(response.text)
-        if response.status_code == 200 and resData["code"] == 200:  #  请求成功
+        if response.status_code == 200 and resData["code"] == 200:  # 请求成功
             if requestCount > 0:
                 self.textLog("尝试请求成功", "success")
             return resData
-        if response.status_code == 401 or resData["code"] == 401:  # 401 重新写获取token
-            # TODO token获取操作 只操作一次
-            self.getToken()
+        if response.status_code == 401 or resData["code"] == 401:  # 401 重新写获取token 只执行一次的Token获取
+            if not self.dewuRequestAgainToken:
+                self.dewuRequestAgainToken = True
+                self.textLog("Token失效尝试重新获取", "warning")
+                self.token = self.authLogin()
+                return self.dewuRequest(method, url, data, requestCount)
+            else:
+                logging.error("[接口][请求失败][Token重新获取失效]" + response.text)
+                self.textLog("请求失败：" + resData["msg"], "error")
+                return False
         else:
-            logging.error("接口请求失败：" + response.text)
+            logging.error("[接口][请求失败]" + response.text)
             # 文本提示写入，重新请求获取
             if requestCount < self.dewuRequestMax:
                 self.textLog("查询接口错误，尝试3次重新请求，第 " + str(requestCount + 1) + " 次", "warning")
                 time.sleep(self.dewuRequestWaitTime)  # 等待下次请求时间
                 requestCount += 1
-                self.dewuRequest(method, url, data, requestCount)
+                return self.dewuRequest(method, url, data, requestCount)
             else:
-                self.textLog("请求失败", "error")
+                logging.error("[接口][请求失败]" + "多次请求失败")
+                self.textLog("请求失败：" + resData["msg"], "error")
                 return False
 
     # GET 请求
     def __get(self, url, params):
         params = self.__request(params)
-        logging.info(self.api + url, params, self.__headers())  # 日志记录请求
-        res = requests.get(self.api + url, params=params,
-                           headers=self.__headers())
+        logging.info(
+            "[接口][请求][请求地址]" + self.api + url + "[请求参数]" + str(params) + "[请求头]" + str(self.__headers()))  # 日志记录请求
+        res = requests.get(self.api + url, params=params, headers=self.__headers())
         return res
 
     # POST 请求
     def __post(self, url, data):
         data = self.__request(data)
-        logging.info(self.api + url, data, self.__headers())  # 日志记录请求
+        logging.info(
+            "[接口][请求][请求地址]" + self.api + url + "[请求参数]" + str(data) + "[请求头]" + str(self.__headers()))  # 日志记录请求
         res = requests.post(
             self.api + url, data=json.dumps(data), headers=self.__headers())
         return res
